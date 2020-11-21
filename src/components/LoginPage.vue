d9ef204187b8d3ba673ebb3a481ce954ed542179
kasparkadalipp <kaspar.kadalipp@gmail.com>
2020-11-21 19:29:13 +0200

    Initial layout

---
+++ src/components/LoginPage.vue
@@ -0,0 +1,44 @@
+<template>
+  <div>
+    <div>
+      <h1>Welcome to postIt!</h1>
+    </div>
+    <div>
+      <h4>
+        <a href="#">Create an Account</a>
+      </h4>
+    </div>
+    <div>
+      <h4>Or</h4>
+    </div>
+    <div>
+      <h3>Please Log In</h3>
+      <form action="index.html" method="post">
+        <div>
+          <input type="text" name="email" placeholder="Email">
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="Password">
+        </div>
+        <div>
+          <button class="button" type="submit" name="login">Log In</button>
+        </div>
+        <div>
+          <small>
+            <a href="#">Forgot password?</a>
+          </small>
+        </div>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "LoginPage"
+}
+</script>
+
+<style scoped>
+
+</style>
