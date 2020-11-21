6988e62b4a771cfcb90f87327953635754763b5b
kasparkadalipp <kaspar.kadalipp@gmail.com>
2020-11-21 20:05:59 +0200

    Initial layout

---
+++ src/components/BrowseItem.vue
@@ -0,0 +1,79 @@
+<template>
+  <div class="profile">
+    <div class="mask">
+      <img :src="avatar" alt="User">
+    </div>
+    <p>{{ fullName }}</p>
+    <div class="browse-actions">
+      <button type="button" name="like" class="follow-button">Follow</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "BrowseItem",
+  props: {
+    firstname: String,
+    lastname: String,
+    avatar: String
+  },
+  computed: {
+    fullName: function () {
+      return this.firstname + " " + this.lastname
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.profile {
+  display: inline-block;
+  background-color: #fefefe;
+  border-style: groove;
+  border-width: 1px;
+  border-radius: 5px;
+  padding: 2px;
+}
+
+.profile p {
+  text-align: center;
+  font-weight: bold;
+  font-size: larger;
+}
+
+.mask {
+  margin: 15px auto;
+  display: block;
+  width: 100px;
+  height: 100px;
+  border-radius: 50%;
+  overflow: hidden;
+}
+
+.mask img {
+  max-width: 100%;
+  min-height: 100%;
+  object-fit: cover;
+}
+
+.profile button {
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+}
+
+.follow-button {
+  background-color: #82008f;
+  color: white;
+
+}
+
+.follow-button.followed {
+  background-color: #fefefe;
+  color: #82008f;
+  border: 1px #82008f solid;
+  border-radius: 4px;
+}
+</style>
---
+++ src/components/BrowsePage.vue
@@ -0,0 +1,54 @@
+<template>
+  <main>
+    <TheHeader/>
+    <BrowseItem
+        class="profile-container"
+        v-for="profile in profiles"
+        v-bind="profile"
+        :key="profile.id">
+    </BrowseItem>
+
+  </main>
+</template>
+
+<script>
+import TheHeader from "@/components/TheHeader";
+import BrowseItem from "@/components/BrowseItem";
+import {mapState} from 'vuex';
+
+export default {
+  name: "BrowsePage",
+  components: {BrowseItem, TheHeader},
+  computed: mapState({
+    profiles: state => state.profiles
+  }),
+  mounted() {
+    this.$store.dispatch("loadPosts");
+  }
+}
+</script>
+
+<style scoped>
+
+body {
+  height: 100%;
+  padding: 0;
+  margin: 0;
+}
+
+main {
+  width: 50%;
+  min-width: 450px;
+  min-height: 100%;
+  margin: auto auto;
+  padding: 90px 15px 15px 15px;
+  background-color: #ffffff;
+}
+
+.profile-container {
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-gap: 15px;
+}
+
+</style>
