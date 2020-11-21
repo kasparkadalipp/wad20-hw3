f8f5982a041d81855e03a03b55fff7b023ea381e
kasparkadalipp <kaspar.kadalipp@gmail.com>
2020-11-21 21:22:05 +0200

    Add state mutations

--- src/store/index.js
+++ src/store/index.js
@@ -35,12 +35,18 @@ const mutations = {
     setPosts(state, posts) {
         state.posts = posts
     },
+    toggleLiked(state, id){
+        state.posts = state.posts.map(post => post.id === id ? {...post, id:!post.liked} : post)
+    },
     setUserProfile(state, user) {
         state.user = user
     },
     setProfiles(state, profiles) {
         state.profiles = profiles
     },
+    toggleFollowed(state, id){
+        state.profiles = state.profiles.map(profile => profile.id === id ? {...profile, id:!profile.followed} : profile)
+    }
 }
 
 export default new Vuex.Store({
