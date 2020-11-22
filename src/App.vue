3a9ca4ae2101dfc08e7c365637388774015bd339
kasparkadalipp <kaspar.kadalipp@gmail.com>
2020-11-22 23:23:30 +0200

    Refactor code

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,18 @@
 <template>
   <v-app style="background: #0277bd">
-    <TheHeader v-if="$route.fullPath !== '/login'"></TheHeader>
     <router-view></router-view>
   </v-app>
 
 </template>
 
 <script>
-
-import TheHeader from "@/components/TheHeader";
-
-export default {
-  name: 'App',
-  components: {TheHeader},
-  created() {
-    this.$router.push("/posts");
-    this.$store.dispatch("loadProfiles");
-    this.$store.dispatch("loadPosts");
-    this.$store.dispatch("loadUserProfile");
-  }
-};
+  export default {
+    name: 'App',
+    created() {
+      this.$router.push("/posts");
+      this.$store.dispatch("loadProfiles");
+      this.$store.dispatch("loadPosts");
+      this.$store.dispatch("loadUserProfile");
+    }
+  };
 </script>
